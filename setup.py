687462ce756fea155857d6dea35db29fac55a7d9
Carlos Gomez <carlosgg33@gmail.com>
2019-03-18 12:09:52 +0100

    creating a setup.py file

---
+++ setup.py
@@ -0,0 +1,49 @@
+# !/usr/bin/env python
+
+
+
+from distutils.core import setup
+
+setup(
+
+    name='hciplot',
+
+    packages=['hciplot'],
+
+    version='0.1.0',
+
+    description='High-contrast Imaging Plotting library',
+
+    author='Carlos Alberto Gomez Gonzalez',
+
+    license='MIT',
+
+    author_email='carlosgg33@gmail.com',
+
+    url='https://github.com/carlgogo/hciplot',
+
+    keywords=['plotting', 'hci', 'package'],
+
+    classifiers=[
+
+        'Development Status :: 4 - Beta',
+
+        'Environment :: Console',
+
+        'Intended Audience :: Developers',
+
+        'Natural Language :: English',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python',
+
+        'Programming Language :: Python :: 3.7',
+
+        'Programming Language :: Python :: Implementation :: PyPy',
+
+        'Topic :: Software Development',
+
+    ],
+
+)
