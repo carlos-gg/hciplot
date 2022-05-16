b3dbf18b7a1bd34ec1596056751920d1ebc24fa4
VChristiaens <valentinchrist@hotmail.com>
2022-05-16 16:52:21 +0200

    Updated setup.py to reflect updated behaviour of parse_requirements - used during automatic installation of dependencies

--- setup.py
+++ setup.py
@@ -3,21 +3,33 @@
 import os
 import re
 from setuptools import setup
-try:  # pip >= 10
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
-except ImportError:  # pip <= 9.0.3
-    from pip.req import parse_requirements
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
 def resource(*args):
     return os.path.join(os.path.abspath(os.path.join(__file__, os.pardir)),
                         *args)
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-reqs = parse_requirements(resource('requirements.txt'), session=False)
-try:
-    reqs = [str(ir.req) for ir in reqs]
-except:
-    reqs = [str(ir.requirement) for ir in reqs]
+reqs = parse_requirements(resource('requirements.txt'), session=PipSession)
+requirements = [str(ir.requirement) for ir in reqs]    
+
+reqs_dev = parse_requirements(resource('requirements-dev.txt'), 
+                              session=PipSession)
+requirements_dev = [str(ir.requirement) for ir in reqs_dev]  
 
 
 with open(resource('hciplot', '__init__.py')) as version_file:
@@ -42,7 +54,7 @@ setup(
     url='https://github.com/carlgogo/hciplot',
     download_url='https://github.com/carlgogo/hciplot/archive/refs/tags/v0.1.8.tar.gz',
     keywords=['plotting', 'hci', 'package'],
-    install_requires=reqs,
+    install_requires=requirements,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
